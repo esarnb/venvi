8844d98a33632c0c952f0c2e7a2ddf44345ea082
Esar Behlum <esarnb@gmail.com>
2019-07-31 15:37:27 -0700

    Updates

---
+++ precache-manifest.a0098467a8f3fa198f45fc1ff5d3d8c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cebba30839317436c50f7db6c18c201",
+    "url": "/venvi/index.html"
+  },
+  {
+    "revision": "73199ab81306530ed0c1",
+    "url": "/venvi/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/venvi/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "73199ab81306530ed0c1",
+    "url": "/venvi/static/js/main.283a03d9.chunk.js"
+  },
+  {
+    "revision": "9eda165e4c1b4731b8ea",
+    "url": "/venvi/static/js/runtime~main.ee53c1b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/venvi/static/media/logo.5d5d9eef.svg"
+  }
+]);
